6c91942d5a12f1d1f69dcf1b1a41002f25559371
kat-atat <kat_atat@live.jp>
2017-04-16 11:36:32 +0900

    fix access

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 {
   let appName = "testapp";
-  let appURL = "https://kat-atat.github.io/temp";
+  let appURL = "https://kat-atat.github.io/temp.html";
 
   let libodon = new Libodon(appName, appURL);
 
