c36a7c3c2bbbe794d110190db2d7c98336379b79
*nikki <64@live.jp>
2019-03-07 17:23:50 +0900

    progress

--- src/storage.js
+++ src/storage.js
@@ -1,6 +1,7 @@
 const storage = {
   get: (key)=> JSON.parse(localStorage.getItem(key)),
   set: (key, obj)=> localStorage.setItem(key, JSON.stringify(obj)),
+  remove: (key)=> localStorage.removeItem(key),
   clear: ()=> localStorage.clear(),
 }
 
